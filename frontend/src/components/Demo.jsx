4683e872430d5d198ab1a919cf063d8bba9a796c
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-02-25 17:18:58 +0545

    Update PrivateComponent and Nav components

---
+++ frontend/src/components/Demo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Demo = () => {
+    return (
+        <div>
+           This is a demo component
+        </div>
+    );
+}
+
+export default Demo;
--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,39 +1,76 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from 'react-router-dom';
-import ProductList from './ProductList'; 
+import ProductList from "./ProductList";
 
-const Nav=()=>{
-    const [showProductList, setShowProductList] = useState(false);
-
+const Nav = () => {
+    const [clickImage, setClickImage] = useState(false);
     const auth = localStorage.getItem('user');
     const navigate = useNavigate();
-    const logout =()=>{
+
+
+    const logout = () => {
         localStorage.clear();
-        navigate('/signup');
+        navigate('/');
     }
 
-    return(
+    const handleLoginClick = () => {
+        setClickImage(false);
+    }
+
+    const handleSignupClick = () => {
+        setClickImage(false);
+    }
+
+    const handleClickImage = () => {
+        navigate('/');
+
+        setClickImage(true);
+       
+    }
+
+    return (
         <div>
-            <img src="https://w7.pngwing.com/pngs/621/196/png-transparent-e-commerce-logo-logo-e-commerce-electronic-business-ecommerce-angle-text-service.png" 
-            alt="logo" 
-            className="logo" 
-             onClick={() => setShowProductList(true)}/>
-             {showProductList && <ProductList />}
-            { auth ?<ul className="nav-ul">
-                <li><Link to="/">Products</Link></li>
-                <li><Link to="/add">Add Product</Link></li>
-                <li><Link to="/update">Update Product</Link></li>
-                <li><Link to="/profile">Profile</Link></li>
-                <li><Link onClick={logout} to='/signup'>Logout ({JSON.parse(auth).name})</Link></li>
+            {/* <img 
+                src="https://w7.pngwing.com/pngs/621/196/png-transparent-e-commerce-logo-logo-e-commerce-electronic-business-ecommerce-angle-text-service.png" 
+                alt="logo" 
+                className="logo" 
+                onClick={() => setClickImage(true)}
+            /> */}
+            {auth ? (
+                <ul className="nav-ul">
+                    <li><img 
+                src="https://w7.pngwing.com/pngs/621/196/png-transparent-e-commerce-logo-logo-e-commerce-electronic-business-ecommerce-angle-text-service.png" 
+                alt="logo" 
+                className="logo" 
+                onClick={() => setClickImage(true)}
+            /></li>
+                    <li><Link to="/">Products</Link></li>
+                    <li><Link to="/add">Add Product</Link></li>
+                    <li><Link to="/update">Update Product</Link></li>
+                    <li><Link to="/profile">Profile</Link></li>
+                    <li><Link onClick={logout} to='/signup'>Logout ({JSON.parse(auth).name})</Link></li>
                 </ul>
-                :
-                <ul className="nav-ul nav-right">
-                    <li><Link to ="/signup">Sign Up</Link></li>
-                    <li><Link to ="/login">Login</Link></li>
+            ) : (
+                <div>
+                  <img 
+                src="https://w7.pngwing.com/pngs/621/196/png-transparent-e-commerce-logo-logo-e-commerce-electronic-business-ecommerce-angle-text-service.png" 
+                alt="logo" 
+                className="logo" 
+                onClick={() => handleClickImage()}
+                
+            /> 
+
+                     <ul className="nav-ul nav-right">
+                    <li><Link  onClick={handleLoginClick} to="/login">Login</Link></li>
+                   <li><Link  onClick={handleSignupClick} to="/signup">Signup</Link></li>
+
                 </ul>
-               }   
+                {clickImage && <ProductList />}
+                </div>
+               
+            )}
         </div>
     )
 }
- 
+
 export default Nav;
