2c589508c595dd4668d4e692e59c60185d72fe09
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-02-27 17:29:11 +0545

    Update MongoDB connection in config.js

--- backend/models/config.js
+++ backend/models/config.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://biwash10:biwash20@cluster0.m1wcyog.mongodb.net/EcomDashboard");
-//mongoose.connect("mongodb+srv://biwash10:biwash10@cluster0.m1wcyog.mongodb.net/ecommerce");
-
+mongoose.connect("mongodb+srv://biwash10:biwash20@cluster0.m1wcyog.mongodb.net/EcomDashboard")
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.log('Failed to connect to MongoDB:', error));
