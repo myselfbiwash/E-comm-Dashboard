ebba9fd36c2aa2b30d67a333f2aff779022800cb
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-02-27 17:29:25 +0545

    Add token verification middleware to route.js

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/Product");
 const {upload} = require("../middleware/Multer");
+const { verifyToken } = require("../middleware/TokenVerification");
 
 const {
     handleDeleteProduct,
@@ -9,6 +10,6 @@ const {
     handleUpdateProduct,
 } = require("../controllers/controller");
 
-router.route("/:id").get(handleGetOneProduct).put(upload.single('photo'),handleUpdateProduct).delete(handleDeleteProduct);
+router.route("/:id").get(handleGetOneProduct).put(verifyToken,upload.single('photo'),handleUpdateProduct).delete(verifyToken,handleDeleteProduct);
 
 module.exports = router;
