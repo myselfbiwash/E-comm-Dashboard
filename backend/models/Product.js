c7664eceb8ff79b18a741b4bbd38f352b5ed9ee4
Biwash Budhathoki <075bct025@ioepc.edu.np>
2024-02-27 18:22:24 +0545

    Refactor productSchema in Product.js in Detail

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,20 +1,39 @@
-const { ObjectId } = require('mongodb');
-const mongoose = require('mongoose');
-
 const productSchema = new mongoose.Schema({
-    name:String,
-    price:String,
-    category:String,
-    company:String,
-    userId: ObjectId,
-    number: Number,
-    photo: String,
-    pid: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-
+  name: {
+      type: String,
+      required: true,
+      trim: true
+  },
+  price: {
+      type: Number,
+      required: true,
+      min: 0
+  },
+  category: {
+      type: String,
+      required: true,
+      trim: true
+  },
+  company: {
+      type: String,
+      required: true,
+      trim: true
+  },
+  userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'users'
+  },
+  number: {
+      type: Number,
+      required: true,
+      min: 0
+  },
+  photo: {
+      type: String,
+  },
+  pid: {
+      type: String,
+      required: true,
+  }
 });
-
-module.exports = mongoose.model("products",productSchema);
